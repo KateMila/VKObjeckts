01171f4582128a4c35c8cae718df0988dc8e1af9
KateMila <89808903+KateMila@users.noreply.github.com>
2022-03-14 23:24:03 +0300

    Second commit

--- src/main/java/ru/netology/domain/CommentsInfo.java
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -2,9 +2,9 @@ package ru.netology.domain;
 
 public class CommentsInfo {
     private String urlName;
-    private int countComment;
-    private int likePost;
-    private int replyPostId;
+    private boolean countComment;
+    private boolean likePost;
+    private boolean replyPostId;
     private int replyPostGroup;
 
     // + get/set на все поля
---
+++ src/main/java/ru/netology/domain/ManagerInfo.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+public class ManagerInfo {
+    private NewPost newPost;
+    private CommentsInfo commentsInfo;
+    private Donut donut;
+    private Likes likes;
+    private Views views;
+
+
+    // + get/set на все поля
+}
