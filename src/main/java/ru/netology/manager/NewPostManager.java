70dda3487d6a83d84cbb6dc90404bcef3743744a
KateMila <89808903+KateMila@users.noreply.github.com>
2022-03-29 19:04:53 +0300

    Added manager

---
+++ src/main/java/ru/netology/manager/NewPostManager.java
@@ -0,0 +1,12 @@
+package ru.netology.manager;
+
+import ru.netology.domain.NewPost;
+
+public class NewPostManager {
+    private NewPost[] newPosts;
+
+    public NewPost[] getNewPosts(){
+        // To do some logic;
+        return null;
+    }
+}
