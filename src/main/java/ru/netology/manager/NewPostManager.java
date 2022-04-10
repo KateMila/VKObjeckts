f0a7aa81a76ae779af308fd4e3524fa28c982954
KateMila <89808903+KateMila@users.noreply.github.com>
2022-04-10 09:57:51 +0300

    Second update Manager

--- src/main/java/ru/netology/manager/NewPostManager.java
+++ src/main/java/ru/netology/manager/NewPostManager.java
@@ -4,15 +4,39 @@ import ru.netology.domain.*;
 
 
 public class NewPostManager {
-    private NewPost[] newPosts;
-    private Views[] views;
-    private Likes[] likes;
-    private Donut[] donut;
-    private CommentsInfo[] commentsInfo;
-    private Reposts[] reposts;
+    NewPost newPost = new NewPost();
+    Views view = new Views();
+    Likes like = new Likes();
+    Donut donut = new Donut();
+    CommentsInfo comment = new CommentsInfo();
+    Reposts repost = new Reposts();
 
+    public NewPost getNewPost() {
+        // To do some logic;
+        return null;
+    }
 
-    public NewPost[] getNewPosts() {
+    public Views getView() {
+        // To do some logic;
+        return null;
+    }
+
+    public Likes getLike() {
+        // To do some logic;
+        return null;
+    }
+
+    public Donut getDonut() {
+        // To do some logic;
+        return null;
+    }
+
+    public CommentsInfo getComment() {
+        // To do some logic;
+        return null;
+    }
+
+    public Reposts getRepost() {
         // To do some logic;
         return null;
     }
